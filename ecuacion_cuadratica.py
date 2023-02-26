4ca6a21d0663ed358ea83f2b93773512df4c5467
AndresFelipeJW <andresjimenez@colegioguanenta.edu.co>
2023-02-26 14:37:10 -0500

    Se agrego el diagrama de flujo, programa y el README

---
+++ ecuacion_cuadratica.py
@@ -0,0 +1,11 @@
+# PROGRAMA PARA RESOLVER UNA ECUACIÓN CUADRATICA 
+
+print("--------------------------------")
+print("------ INGRESE UN NUMERO -------")
+print("--------------------------------")
+
+#input
+x = int(input("INGRESE EL VALOR DE X: "))
+y = int(input("INGRESE EL VALOR DE Y: "))
+
+#processing
